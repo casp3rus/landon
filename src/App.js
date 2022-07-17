a1572ba5d288380e551f83da5401b1e8fac822fc
Adrian M. Popan <adrian.popan@gmail.com>
2022-07-17 16:57:33 -0500

    test change for AWS Amplify

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React - LinkedIn React and AWS course
         </a>
       </header>
     </div>
